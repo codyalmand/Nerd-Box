dcaba235913d1264b7b3bfb86e7c3b572ca5dbaf
Austin Lambert <atlambert11@gmail.com>
2020-10-06 15:03:43 -0500

    Added object containing video game and movies array

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-
 var gameTitle = "Death Stranding";
 var queryURL = "https://api.rawg.io/api/games?search=" + gameTitle;
+
+var movieGameList = {
+    happy: {
+        games: ["Stardew Valley", "Animal Crossing: New Horizons", "Fall Guys", "Mario Kart 8"],
+        movies: ["Up", "My Neighbor Totoro", "School of Rock", "Inside Out"],
+    },
+    sad: {
+        games: ["What Remains of Edith Finch", "Night In The Woods", "To the Moon", "The Legend of Zelda: Link's Awakening"],
+        movies: ["Titanic", "Les Miserables", "Eternal Sunshine for the Spotless Mind", " Her"],
+    },
+    energetic: {
+        games: ["Marvel's Spider-Man", "Superhot", "DOOM Eternal", "Gears of War"],
+        movies: ["Mad Max: Fury Road", "Baby Driver", "John Wick", "300"],
+    },
+    funny: {
+        games: ["Portal", "Portal 2", "BattleBlock Theater", "Battletoads"],
+        movies: ["Just Friends", "Sorority Boys", "Shaun of the Dead", "Hot Fuzz"],
+    },
+    scared: {
+        games: ["Alien: Isolation", "Dead Space", "Silent Hill", "Resident Evil 7: Biohazard"],
+        movies: ["Midsommar", "Mother", "It", "The Conjuring"],
+    },
+    lonely: {
+        games: ["Lovers in a Dangerous Spacetime", "Overcooked 2", "Apex Legends", "Gang Beast"],
+        movies: ["The Notebook", "Casablanca", "The Vow", "Dirty Dancing"],
+    },
+};
 
 
 $.ajax({
     url: queryURL,
     method: "GET"
-}).then(function (response) {
+}).then(function(response) {
     //console.log(response);
     var name = response.results[0].name;
     var backgroundImage = response.results[0].background_image;
@@ -32,7 +58,7 @@ var queryMovies = "https://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&a
 $.ajax({
     url: queryMovies,
     method: "GET"
-}).then(function (response) {
+}).then(function(response) {
     console.log(response);
     var movieTitle = response.Title;
     var movieScore = response.Metascore;
@@ -43,4 +69,3 @@ $.ajax({
     var movieReleased = response.Released;
     console.log(movieReleased);
 });
-
