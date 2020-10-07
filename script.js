e063c6fc903b9fc7f6e1fcd5f482b607518905cf
Austin Lambert <atlambert11@gmail.com>
2020-10-07 11:08:23 -0500

    Updated Oject Array

--- script.js
+++ script.js
@@ -3,24 +3,24 @@ var queryURL = "https://api.rawg.io/api/games?search=" + gameTitle;
 
 var movieGameList = {
     happy: {
-        games: ["Stardew Valley", "Animal Crossing: New Horizons", "Fall Guys", "Mario Kart 8"],
-        movies: ["Up", "My Neighbor Totoro", "School of Rock", "Inside Out"],
+        games: ["Stardew Valley", "Animal Crossing: New Horizons", "Fall Guys", "Mario Kart 8", "Legend of Zelda: Breath of the Wild", "Super Mario Odyssey"],
+        movies: ["Up", "My Neighbor Totoro", "School of Rock", "Inside Out", "The Incredibles", "Toy Story"],
     },
     sad: {
-        games: ["What Remains of Edith Finch", "Night In The Woods", "To the Moon", "The Legend of Zelda: Link's Awakening"],
-        movies: ["Titanic", "Les Miserables", "Eternal Sunshine for the Spotless Mind", " Her"],
+        games: ["What Remains of Edith Finch", "Night In The Woods", "To the Moon", "The Legend of Zelda: Link's Awakening", "The Last of Us", "Ghost of Tsushima"],
+        movies: ["Titanic", "Les Miserables", "Eternal Sunshine for the Spotless Mind", " Her", "Armageddon", "Black Hawk Down"],
     },
     energetic: {
-        games: ["Marvel's Spider-Man", "Superhot", "DOOM Eternal", "Gears of War"],
-        movies: ["Mad Max: Fury Road", "Baby Driver", "John Wick", "300"],
+        games: ["Marvel's Spider-Man", "Superhot", "DOOM Eternal", "Gears of War", "Bloodborne", "Final Fantasy VII: Remake"],
+        movies: ["Mad Max: Fury Road", "Baby Driver", "John Wick", "300", "Upgrade", "Dredd"],
     },
     funny: {
-        games: ["Portal", "Portal 2", "BattleBlock Theater", "Battletoads"],
-        movies: ["Just Friends", "Sorority Boys", "Shaun of the Dead", "Hot Fuzz"],
+        games: ["Portal", "Portal 2", "BattleBlock Theater", "Battletoads", "Rocket League", "Among Us"],
+        movies: ["Just Friends", "Sorority Boys", "Shaun of the Dead", "Hot Fuzz", "Walk Hard", "Deadpool"],
     },
     scared: {
-        games: ["Alien: Isolation", "Dead Space", "Silent Hill", "Resident Evil 7: Biohazard"],
-        movies: ["Midsommar", "Mother", "It", "The Conjuring"],
+        games: ["Alien: Isolation", "Dead Space", "Silent Hill", "Resident Evil 7: Biohazard", "Outlast", "Visage"],
+        movies: ["Midsommar", "Mother", "It", "The Conjuring", "Evil Dead (2013)", "Hereditary"],
     },
     lonely: {
         games: ["Lovers in a Dangerous Spacetime", "Overcooked 2", "Apex Legends", "Gang Beast"],
